d51d10a0c9abbad139b7956606d16e30806dd638
Jonathan Ziller <jonathan.ziller@gmail.com>
2015-11-25 20:27:59 +0100

    intro for agents

---
+++ IntroAgent/Program.fs
@@ -0,0 +1,6 @@
+﻿[<EntryPoint>]
+let main argv = 
+    // Solution.run()
+    Template.run()
+    System.Console.ReadKey() |> ignore
+    0 // return an integer exit code
---
+++ IntroAgent/Template.fs
@@ -0,0 +1,17 @@
+﻿module Template
+
+type Agent<'T> = MailboxProcessor<'T>
+
+type Message<'TRequest, 'TResponse> = Message of 'TRequest * AsyncReplyChannel<'TResponse>
+
+let run () =
+    // stateless
+
+    // stateful (mutable)
+    
+    // stateful (immutable)
+
+    // request/response
+
+    ()
+
